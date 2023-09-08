686a2f462379f12bc61d0e7d8133faac3cebc058
gabriel-gamaofc <130513430+gabriel-gamaofc@users.noreply.github.com>
2023-09-08 10:06:56 -0300

    Aula 1 de python
    Aula de introdução ao python

---
+++ py/aula1.py
@@ -0,0 +1,31 @@
+#Instituição: Uninassau
+#Docente: Cloves Rocha
+#Estudante: Gabriel Gama
+
+#2.0Tipos primitivos de dados
+ #g="a"
+ #print("Tipo do dado: ",type(g))
+ #print(type(g))
+
+#3.0operadores
+#t1=10+9 #adição
+#print(t1)
+
+#t2=9*7#Mutiplicação
+#print(t2)
+
+#t3=9-8#Subtração
+#print(t3)
+
+#t4=10//3#Divisão inteira
+#print(t4)
+
+#t5=10/3#Divisão 
+#print(t5)
+
+#t6=10%3 # resto da divisão
+#print(t6)
+
+t7=5**3 #Exponenciação
+print(t7)
+ 
